80929e87f72481877ac98951bf6bd266738f633a
Michael <michael@nextpertise.nl>
2025-10-16 14:51:11 +0200

    Add docker image building

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -f docker/Dockerfile . -t registry.nextpertise.tools/nextpertise/pysim:latest
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm registry.nextpertise.tools/nextpertise/pysim:latest $@
